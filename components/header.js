119c2f83699927bb92f8690bb95e464fab08a152
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-11-30 14:12:49 -0500

    added colors.js and header.js

---
+++ components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { styles } from "../style/style";
+
+export default function TestText() {
+  return (
+    <View style={styles.container}>
+      <Text>wassup</Text>
+    </View>
+  );
+}
