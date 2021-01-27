5198a29f64deac3348436aa5d01ed009d7ed4be2
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2021-01-27 13:20:19 -0500

    removed print statements

--- screens/EmailSignUpScreen.js
+++ screens/EmailSignUpScreen.js
@@ -88,14 +88,8 @@ class EmailSignUpScreen extends Component {
               { cancelable: false }
             );
           }
-          console.log(errorMessage);
           // ..
         });
-      console.log("what it thinks the error message is" + errorMessage);
-      //ERROR DOESNT HAVE TIME TO COME BACK IN TIME, NEED TO DO SOME ASYNC AWAIT SHIT?
-      //OR WE MUST DO OUR OWN PRELIMINARY CHECKS, SUCH AS EMAIL FORMATTING AND PASSWORD MUST BE AT LEAST 6 CHAR
-
-      //SAVE USER INFO TO DBH LIKE WE DID WITH GOOGLE LOGIN
     }
   }
 
