8602c7c9830a6aaef7935ef6be32a78937f872f1
Abdallah Shapsough <abshapsough@gmail.com>
2021-03-16 21:53:18 -0400

    add import firebase

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import "firebase/auth";
 import SetView from "./SetView";
 import { UserProvider } from './Context';
 import dbh from "./firebase";
+import firebase from "firebase";
 
 export default function App() {
   const [user, setUser] = useState({});
