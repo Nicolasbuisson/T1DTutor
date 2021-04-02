22b1bfbb378f20e8c693ce6b66ff63158fb8f663
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2021-04-02 18:04:29 -0400

    initial local notification system

--- App.js
+++ App.js
@@ -1,64 +1,79 @@
 import React, { useEffect, useState } from "react";
 import "firebase/auth";
 import SetView from "./SetView";
-import { UserProvider } from './Context';
+import { UserProvider } from "./Context";
 import firebase from "firebase";
-import {getUser, updateUser} from "./database";
+import { getUser, updateUser } from "./database";
 
 export default function App() {
   const [user, setUser] = useState({});
 
   const [view, setView] = useState("LoginScreen");
 
-  useEffect(()=>{
+  useEffect(() => {
     userAuth();
-  }, []);
-  
-  const userAuth = () => {
+  }, []);
+
+  const userAuth = () => {
     firebase.auth().onAuthStateChanged((currentUser) => {
-      if (currentUser && ((user.uid && currentUser?.uid !== user.uid) || !user.uid)) {
-        getUser(currentUser.uid, (data)=>{
-          if(data) {
-            setUser({...data});
-            if(view === "LoginScreen" && !data?.isNewUser) {
+      if (
+        currentUser &&
+        ((user.uid && currentUser?.uid !== user.uid) || !user.uid)
+      ) {
+        getUser(currentUser.uid, (data) => {
+          if (data) {
+            setUser({ ...data });
+            if (view === "LoginScreen" && !data?.isNewUser) {
               setView("DashboardScreen");
-            } else if(data?.isNewUser) {
+            } else if (data?.isNewUser) {
               setView("LanguageQuestionScreen");
             }
           } else {
             setView("LanguageQuestionScreen");
           }
-        })
+        });
       } else {
-        if(view === "EmailLoginScreen" || view === "EmailSignUpScreen") return;
-        if(view !== "LoginScreen") {
+        if (view === "EmailLoginScreen" || view === "EmailSignUpScreen") return;
+        if (view !== "LoginScreen") {
           setUser({});
           setView("LoginScreen");
         }
       }
     });
-  }
+  };
 
-  const completeQuestions = (user) => {
-    updateUser(user.uid, {
-      questions: {...user.questions},
-      language: user.language,
-      isNewUser: false
-    },
-    ()=>{
-      setView("DashboardScreen");
-    }
-    )
-  }
+  const completeQuestions = (user) => {
+    updateUser(
+      user.uid,
+      {
+        questions: { ...user.questions },
+        language: user.language,
+        isNewUser: false,
+      },
+      () => {
+        setView("DashboardScreen");
+      }
+    );
+  };
 
-  const updateUserAndState = (options, callback) => {
-    if(!user.uid) return;
-    setUser({...user, ...options});
+  const updateUserAndState = (options, callback) => {
+    if (!user.uid) return;
+    setUser({ ...user, ...options });
     updateUser(user.uid, options, callback);
-  }
+  };
 
-  
-  return <UserProvider value={{user,setUser,view,setView,completeQuestions,updateUserAndState}}>
-    <SetView view={view}></SetView>
-  </UserProvider>;
+  return (
+    <UserProvider
+      value={{
+        user,
+        setUser,
+        view,
+        setView,
+        completeQuestions,
+        updateUserAndState,
+      }}
+    >
+      <SetView view={view}></SetView>
+    </UserProvider>
+  );
 }
--- screens/RemindersScreen.js
+++ screens/RemindersScreen.js
@@ -5,20 +5,32 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
-  Alert,
 } from "react-native";
 import colors from "../style/colors.js";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Context from '../Context'
+import Context from "../Context";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 class RemindersScreen extends Component {
   constructor() {
     super();
     this.state = {
-      email: "",
-      password: "",
+      expoPushToken: "",
+      notification: false,
     };
+    //refs
+    this.notificationListener = null;
+    this.responseListener = null;
 
     //functions
     this.goToHome = this.goToHome.bind(this);
@@ -26,6 +38,11 @@ class RemindersScreen extends Component {
     this.goToTrack = this.goToTrack.bind(this);
     this.goToReminders = this.goToReminders.bind(this);
     this.goToMore = this.goToMore.bind(this);
+    this.registerForPushNotificationsAsync = this.registerForPushNotificationsAsync.bind(
+      this
+    );
+    this.schedulePushNotification = this.schedulePushNotification.bind(this);
+    this.cancelAllNotifications = this.cancelAllNotifications.bind(this);
   }
   static contextType = Context;
 
@@ -49,11 +66,100 @@ class RemindersScreen extends Component {
     this.context.setView("TrackingScreen");
   }
 
+  registerForPushNotificationsAsync = async () => {
+    let token;
+    if (Constants.isDevice) {
+      const {
+        status: existingStatus,
+      } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+    return token;
+  };
+
+  schedulePushNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: "Here is the notification body",
+        data: { data: "goes here" },
+      },
+      trigger: {
+        seconds: 3,
+        //to make it repeat follow this template
+        //seconds: 60 * 1,
+        //repeats: true,
+      },
+    });
+  };
+
+  cancelAllNotifications = async () => {
+    Notifications.cancelAllScheduledNotificationsAsync();
+  };
+
+  componentDidMount() {
+    this.registerForPushNotificationsAsync().then((token) => {
+      console.log(token);
+      this.setState({ expoPushToken: token });
+    });
+
+    this.notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        this.setState({ notification: notification });
+      }
+    );
+
+    this.responseListener = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+
+    return () => {
+      Notifications.removeNotificationSubscription(this.notificationListener);
+      Notifications.removeNotificationSubscription(this.responseListener);
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Header title="Reminders"></Header>
-        <View style={styles.fieldsContainer}></View>
+        <View style={styles.fieldsContainer}>
+          <TouchableOpacity
+            style={styles.touchable}
+            onPress={this.schedulePushNotification}
+          >
+            <Text>test push notification</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.touchable}
+            onPress={this.cancelAllNotifications}
+          >
+            <Text>cancel all notifications</Text>
+          </TouchableOpacity>
+        </View>
         <Footer
           page="reminder"
           homeFunction={this.goToHome}
@@ -80,5 +186,15 @@ const styles = StyleSheet.create({
     flex: 8,
     alignItems: "flex-start",
     justifyContent: "center",
+  },
+  touchable: {
+    marginBottom: 10,
+    marginTop: 10,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.secondary,
+    borderColor: colors.grey,
+    borderWidth: 2,
   },
 });
