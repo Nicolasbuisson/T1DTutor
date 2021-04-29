0253dfb4b3d982a501de8f7bbf3069bca28e37f3
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2021-04-29 14:14:09 -0400

    translated more page header

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -1,19 +1,11 @@
 import React, { Component } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  TouchableOpacity,
-  Alert,
-  Button
-} from "react-native";
+import { View, StyleSheet } from "react-native";
 import colors from "../style/colors.js";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Context from '../Context'
-import Greenbutton from "../components/greenButton"
-import * as Linking from 'expo-linking';
+import Context from "../Context";
+import Greenbutton from "../components/greenButton";
+import * as Linking from "expo-linking";
 import firebase from "firebase";
 
 class MoreScreen extends Component {
@@ -26,25 +18,65 @@ class MoreScreen extends Component {
       languageInput: "",
       uid: "",
     };
-
   }
   static contextType = Context;
 
-
-  changeLanguage = () => {
-    let language = this.context.user.language === "English" ? "French" : "English";
-    this.context.updateUserAndState({language});
-  }
+  changeLanguage = () => {
+    let language =
+      this.context.user.language === "English" ? "French" : "English";
+    this.context.updateUserAndState({ language });
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Header title="More"></Header>
+        {this.context.user?.language === "English" && (
+          <Header title="More"></Header>
+        )}
+        {this.context.user?.language === "French" && (
+          <Header title="Paramètres"></Header>
+        )}
         <View style={styles.fieldsContainer}>
-          <Greenbutton title={this.context.user.language === "English" ? "Re-answer questionnaire" : "Changer mes réponses aux questions"} onPress={()=>this.context.setView("Question1screen")} /> 
-          <Greenbutton title={this.context.user.language === "English" ? "Change language" : "Changer la langue"} onPress={this.changeLanguage} /> 
-          <Greenbutton title={this.context.user.language === "English" ? "Contact us" : "Contactez nous"} onPress={this._handlePress} />      
-          <Greenbutton title={this.context.user.language === "English" ? "Sign out" : "Se déconnecter"} onPress={() => firebase.auth().signOut().then(()=>{this.context.setView("LoginScreen")}).catch((e)=>console.log(e))} />
+          <Greenbutton
+            title={
+              this.context.user.language === "English"
+                ? "Re-answer questionnaire"
+                : "Changer mes réponses aux questions"
+            }
+            onPress={() => this.context.setView("Question1screen")}
+          />
+          <Greenbutton
+            title={
+              this.context.user.language === "English"
+                ? "Change language"
+                : "Changer la langue"
+            }
+            onPress={this.changeLanguage}
+          />
+          <Greenbutton
+            title={
+              this.context.user.language === "English"
+                ? "Contact us"
+                : "Contactez nous"
+            }
+            onPress={this._handlePress}
+          />
+          <Greenbutton
+            title={
+              this.context.user.language === "English"
+                ? "Sign out"
+                : "Se déconnecter"
+            }
+            onPress={() =>
+              firebase
+                .auth()
+                .signOut()
+                .then(() => {
+                  this.context.setView("LoginScreen");
+                })
+                .catch((e) => console.log(e))
+            }
+          />
         </View>
         <Footer></Footer>
       </View>
@@ -52,10 +84,9 @@ class MoreScreen extends Component {
   }
 
   _handlePress = () => {
-    Linking.openURL('mailto:melissa-rosina.pasqua@mail.mcgill.ca');
+    Linking.openURL("mailto:melissa-rosina.pasqua@mail.mcgill.ca");
     this.props.onPress && this.props.onPress();
   };
-
 }
 
 export default MoreScreen;
@@ -71,18 +102,5 @@ const styles = StyleSheet.create({
     flex: 8,
     alignItems: "flex-start",
     justifyContent: "center",
-  },
-  input: {
-    borderColor: colors.grey,
-    borderWidth: 1,
-    // flexBasis: "35%",
-    width: 200,
-  },
-  inputView: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 3,
   },
 });
